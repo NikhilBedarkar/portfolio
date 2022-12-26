c639d186e36c0b61def55276b670a29ccde9ee20
Nikhil Bedarkar <bedarkar39@gmail.com>
2022-12-26 13:25:20 +0530

    chnage username: thevolcanick

--- src/app/service/portfolioDetails.ts
+++ src/app/service/portfolioDetails.ts
@@ -1,55 +1,59 @@
-export const certifications=[
-    {
-        name:'Microsoft azure fundamentals',
-        provider:"Microsoft",
-        url:"https://www.credly.com/badges/51659eed-4d44-404f-b5ff-88b76098c7db?source=linked_in_profile"
-    },
-    {
-        name:'Spring Framework',
-        provider : "Udemy",
-        url:"https://drive.google.com/file/d/1SmA857yEeEIVBL27iOQ3D8GJISOSAZdn/view"
-    },
-    {
-        name:'Docker',
-        provider : "Udemy",
-        url:"https://drive.google.com/file/d/1CGrYVQ5nk7IJG9HQYaET99qvKr_ju0bB/view"
-    },
+export const certifications = [
+  {
+    name: 'Microsoft azure fundamentals',
+    provider: 'Microsoft',
+    url: 'https://www.credly.com/badges/51659eed-4d44-404f-b5ff-88b76098c7db?source=linked_in_profile',
+  },
+  {
+    name: 'Spring Framework',
+    provider: 'Udemy',
+    url: 'https://drive.google.com/file/d/1SmA857yEeEIVBL27iOQ3D8GJISOSAZdn/view',
+  },
+  {
+    name: 'Docker',
+    provider: 'Udemy',
+    url: 'https://drive.google.com/file/d/1CGrYVQ5nk7IJG9HQYaET99qvKr_ju0bB/view',
+  },
 ];
 
-export const projectsToDisplay=[
-    {
-        name:"ONLINE SHOP",
-        technologies:["Angular","Firebase RTDB"],
-        description:"This is angular project for online shop that sell fresh vegitables.",
-        github:"https://github.com/NikhilBedarkar/online-shop",
-        website:"https://online-shop-3f78d.web.app/",
-    },
-    {
-        name:"CRYPTO TRACKER",
-        technologies:["React"],
-        description:"Single page react application to view crypto currency prices live using third party apis.",
-        github:"https://github.com/NikhilBedarkar/crypto-tracker",
-        website:"https://nikhilbedarkar.github.io/crypto-tracker/",
-    },
-    {
-        name:"TIC-TAC-TOE",
-        technologies:["React"],
-        description:"tic-tac-toe game with history of moves made.",
-        github:"https://github.com/NikhilBedarkar/tic-tac-toe",
-        website:"https://nikhilbedarkar.github.io/tic-tac-toe/",
-    },
-    {
-        name:"TodoList",
-        technologies:["React"],
-        description:"learning react demo project.",
-        github:"https://github.com/NikhilBedarkar/TodoList",
-        website:"",
-    },
-    {
-        name:"Virtual Keyboard",
-        technologies:["JavaScript","HTML","CSS"],
-        description:"Virtual Keyboard and graphical password (strategy used to avoid shoulder surfing.)",
-        github:"https://github.com/NikhilBedarkar/Virtual-Keyboard-and-graphical-password",
-        website:"",
-    }
+export const projectsToDisplay = [
+  {
+    name: 'ONLINE SHOP',
+    technologies: ['Angular', 'Firebase RTDB'],
+    description:
+      'This is angular project for online shop that sell fresh vegitables.',
+    github: 'https://github.com/thevolcanick/online-shop',
+    website: 'https://online-shop-3f78d.web.app/',
+  },
+  {
+    name: 'CRYPTO TRACKER',
+    technologies: ['React'],
+    description:
+      'Single page react application to view crypto currency prices live using third party apis.',
+    github: 'https://github.com/thevolcanick/crypto-tracker',
+    website: 'https://thevolcanick.github.io/crypto-tracker/',
+  },
+  {
+    name: 'TIC-TAC-TOE',
+    technologies: ['React'],
+    description: 'tic-tac-toe game with history of moves made.',
+    github: 'https://github.com/thevolcanick/tic-tac-toe',
+    website: 'https://thevolcanick.github.io/tic-tac-toe/',
+  },
+  {
+    name: 'TodoList',
+    technologies: ['React'],
+    description: 'learning react demo project.',
+    github: 'https://github.com/thevolcanick/TodoList',
+    website: '',
+  },
+  {
+    name: 'Virtual Keyboard',
+    technologies: ['JavaScript', 'HTML', 'CSS'],
+    description:
+      'Virtual Keyboard and graphical password (strategy used to avoid shoulder surfing.)',
+    github:
+      'https://github.com/thevolcanick/Virtual-Keyboard-and-graphical-password',
+    website: '',
+  },
 ];
